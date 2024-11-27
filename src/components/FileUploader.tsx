fc76c1e2e966e6f8b6b0607d6d7a72f682e2561d
Danny Nguyen <nguyenduongpa@gmail.com>
2024-11-26 18:58:15 -0800

    upload files

--- src/components/FileUploader.tsx
+++ src/components/FileUploader.tsx
@@ -1,15 +1,69 @@
+import axios from 'axios';
 import React, { ChangeEvent, useState } from 'react'
 
 const FileUploader = () => {
     const [file, setFile] = useState<File | null>(null);
+    const [status, setStatus] = useState<UploadStatus>("idle");
+
+    type UploadStatus = "idle" | "uploading" | "success" | "error";
 
     function handleFileChange(e: ChangeEvent<HTMLInputElement>) {
+        if (e.target.files) {
+            setFile(e.target.files[0]);
+        }
+    }
+
+    async function handleFileUpload() {
+        if (!file) return;
+
+        setStatus("uploading");
+
+        const formData = new FormData();
+        formData.append("file", file);
+
+        try {
+            await axios.post("https://httpbin.org/post", formData, {
+                headers: {
+                    'Content-Type': 'multipart/form-data',
+                }
+            });
+
+            setStatus("success");
+        } catch {
+            setStatus("error");
+        }
 
     }
 
     return (
         <>
             <input type="file" onChange={handleFileChange}/>
+
+            {file && (
+                    <div>
+                        <p>Name: {file.name}</p>
+                        <p>Size: {(file.size / 1024).toFixed(2)} KB</p>
+                        <p>Type: {file.type}</p>
+                    </div>
+            )}
+
+            {file && status !== "uploading" && (
+                <button onClick={handleFileUpload}>
+                    Upload
+                </button>
+            )}
+
+            {status === 'success' && (
+                <p>
+                    File uploaded successfully
+                </p>
+            )}
+
+            {status === 'error' && (
+                <p>
+                    Upload failed. Try again.
+                </p>
+            )}
         </>
     )
 }
